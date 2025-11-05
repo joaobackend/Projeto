4f7598d78d7f39e7f95c1deecfaecd68453cd6c9
joao-pedro13134 <jpdeza11@gmail.com>
2025-11-05 16:50:23 -0300

    correção

--- src/main/java/br/com/senai/backend/estoque/models/Material.java
+++ src/main/java/br/com/senai/backend/estoque/models/Material.java
@@ -1,11 +1,12 @@
 package br.com.senai.backend.estoque.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-@Table
+@Table(name="material")
 @Entity
 public class Material {
    @Id
@@ -26,6 +27,43 @@ public class Material {
 
    }
 
-      public Material(int ano_fabricacao, Integer id, String marca, )
+   public Material(Integer id, String nome, String marca, int anoFabricacao) {
+      this.id = id;
+      this.nome = nome;
+      this.marca = marca;
+      this.anoFabricacao = anoFabricacao;
+   }
+
+   public Integer getId() {
+      return id;
+   }
+
+   public void setId(Integer id) {
+      this.id = id;
+   }
+
+   public String getNome() {
+      return nome;
+   }
+
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
+
+   public String getMarca() {
+      return marca;
+   }
+
+   public void setMarca(String marca) {
+      this.marca = marca;
+   }
+
+   public int getAnoFabricacao() {
+      return anoFabricacao;
+   }
+
+   public void setAnoFabricacao(int anoFabricacao) {
+      this.anoFabricacao = anoFabricacao;
+   }
 
 }
--- src/main/java/br/com/senai/backend/estoque/models/Usuario.java
+++ src/main/java/br/com/senai/backend/estoque/models/Usuario.java
@@ -2,10 +2,82 @@ package br.com.senai.backend.estoque.models;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Table(name="usuario")
+@Entity
 public class Usuario {
-    private Integer id; 
-    private String nome; 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    
+    @Column(name="nome") 
+    private String nome;
+    
+    @Column(name="cargo")
     private String cargo;
-    private LocalDate data_nascimento;
+
+
+    @Column(name="data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name="cpf")
     private String cpf;
+
+    public Usuario(Integer id, String nome, String cargo, LocalDate dataNascimento, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+
+  
 }
